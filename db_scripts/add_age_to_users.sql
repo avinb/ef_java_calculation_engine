628af9a9119d93ca53e7aa1dd51269bae256fc4e
tolomaus <niek.bartholomeus@gmail.com>
2015-10-07 10:35:33 +0200

    Add new field age EF-48 - Oracle syntax - fix issue

--- db_scripts/add_age_to_users.sql
+++ db_scripts/add_age_to_users.sql
@@ -1,6 +1,4 @@
 WHENEVER SQLERROR EXIT SQL.SQLCODE
-BEGIN
-    alter table users add age int;
-    update users set age = 39 where last_name = 'Bartholomeus';
-END;
+alter table users add age int;
+update users set age = 39 where last_name = 'Bartholomeus';
 
