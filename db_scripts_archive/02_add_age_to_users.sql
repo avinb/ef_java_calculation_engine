589b4ce360d81718c2628d0b6c319b9754eaac89
tolomaus <niek.bartholomeus@gmail.com>
2016-01-06 14:12:17 +0100

    Fix issues with executing the db scripts

--- db_scripts_archive/02_add_age_to_users.sql
+++ db_scripts_archive/02_add_age_to_users.sql
@@ -2,3 +2,5 @@ WHENEVER SQLERROR EXIT SQL.SQLCODE
 alter table users add age int;
 update users set age = 39 where last_name like 'Bartholomeus%';
 
+QUIT;
+/
--- db_scripts_archive/03_add_country_to_users.sql
+++ db_scripts_archive/03_add_country_to_users.sql
@@ -1,3 +1,6 @@
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 alter table users add country varchar2(255);
 update users set country = 'Belgium' where first_name = 'Niek';
+
+QUIT;
+/
