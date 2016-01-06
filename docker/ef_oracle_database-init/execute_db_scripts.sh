24be394997b6a52b18023594f73c031586803fd3
tolomaus <niek.bartholomeus@gmail.com>
2016-01-06 20:05:43 +0100

    Fix  trigger by using "as sysdba" connection

--- docker/ef_oracle_database-init/execute_db_scripts.sh
+++ docker/ef_oracle_database-init/execute_db_scripts.sh
@@ -5,7 +5,7 @@ echo "Starting the database..."
 echo "Deploying the db scrips..."
 for i in $( ls -d /db_scripts/* | sort -n ); do
     echo "sqlplus -S system/oracle @$i";
-    cat $i | sqlplus -S system/oracle
+    cat $i | sqlplus -S sys/oracle as sysdba
 done;
 
 echo "Stopping the database..."
