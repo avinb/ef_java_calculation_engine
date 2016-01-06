549c0af84fca8161008b8a6c43d4eca15a5a9586
tolomaus <niek.bartholomeus@gmail.com>
2016-01-06 13:06:30 +0100

    Try db init with another Docker base image

--- docker/ef_oracle_database-init/execute_db_scripts.sh
+++ docker/ef_oracle_database-init/execute_db_scripts.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+#echo "Copying the db data files from /db_data_files (inside the image) to /u01/app/oracle/oradata (volume)..."
+#cp -R /u01/app/oracle/oradata/* /db_data_files
+
 echo "Starting the database..."
 /etc/init.d/oracle-xe start
-
-sleep 10
 
 echo "Deploying the db scrips..."
 for i in $( ls -d /db_scripts/* | sort -n ); do
     echo "sqlplus $i";
-    sqlplus sys/oracle as sysdba @$1
+    sqlplus -S system/oracle@localhost @$1"
 done;
 
 echo "Stopping the database..."
 /etc/init.d/oracle-xe stop
+
+#echo "Copying the db data files from /u01/app/oracle/oradata (volume) to /db_data_files (inside the image)..."
+#cp -R /u01/app/oracle/oradata/* /db_data_files
